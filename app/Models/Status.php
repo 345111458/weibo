691e73339e3c2975358062db34774c14399e4197
Your Name <345111458@qq.com>
2019-04-17 05:11:31 +0000

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -4,12 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-
 class Status extends Model{
+
+    protected $fillable = ['content'];
     //
     // 多对多 绑定模型
     public function user(){
