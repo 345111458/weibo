42775963552a900fbcf453c1010eecc68c1003c5
Your Name <345111458@qq.com>
2019-04-17 09:14:26 +0000

    关注和粉丝列表页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -142,6 +142,25 @@ class UsersController extends Controller{
     }
 
 
+    //显示用户关注人列表
+    public function followings(User $user){
+        $users = $user->followings()->paginate(30);
+        $title = $user->name . '关注的人';
+        return view('users.show_follow',compact('users','title'));
+    }
+
+
+    //用户显示粉丝列表
+    public function followers(User $user){
+        $users = $user->followers()->paginate(30);
+        $title = $user->name . '的粉丝';
+        return view('users.show_follow',compact('users','title'));
+    }
+
+
+
+
+
 
 
 
